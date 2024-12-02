a345d3f9376158c65d3d719b9a47b99155683c31
Matheus Rodrigues <matheusrodrigueserb@outlook.com>
2024-12-02 12:17:05 -0300

    Add project files.

---
+++ TitleToBranchName/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using TextCopy;
+
+string title = "";
+Console.Title = "Title to Branch Name Converter";
+
+while (string.IsNullOrWhiteSpace(title))
+{
+    Console.WriteLine("Paste your Title:");
+    title = Console.ReadLine();
+}
+
+string branchName = title
+    .Replace(" ", "-")
+    .Replace(",", "")
+    .Replace(".", "")
+    .ToLower();
+
+branchName = branchName.Length > 50 ? branchName[.. 50] : branchName;
+ClipboardService.SetText(branchName);
+
+Console.WriteLine(branchName);
+
+Thread.Sleep(5000);
